4440618397102651945c853ac63df34b8e4c7688
KrunalGosai <pwm09627@cuoly.com>
2021-01-07 12:47:43 +0530

    fixed request and order bugs

--- src/app/routes/orders/components/order-view/order-view.component.ts
+++ src/app/routes/orders/components/order-view/order-view.component.ts
@@ -10,7 +10,7 @@ export class OrdersComponentsOrderViewComponent implements OnInit {
 
   constructor(private facade:OrdersFacadeService) { }
   public viewData:any = {};
-  displayedColumns: string[] = ['item_name', 'booked_item_quantity','wastage_item_quantity','suggested_price', 'final_purchase_price','scaling_loss','loss_in_transit'];
+  displayedColumns: string[] = ['item_name', 'booked_item_quantity','item_unit_id','wastage_item_quantity','suggested_price', 'final_purchase_price','scaling_loss','loss_in_transit'];
   dataSource = [];
 
   ngOnInit() {
